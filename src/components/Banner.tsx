ee5570d9cb29590ce67a522b12d768a6770285f5
Kiselev Sergey <kissergey98@mail.ru>
2022-01-29 16:15:24 +0300

    header + banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,12 +2,20 @@ import React from "react";
 
 import classes from './../styles/App/App.module.css';
 
+
+
 export const Banner: React.FC = () => {
 
 
     return (
         <div className={classes.banner}>
-            <h1>h222</h1>
+            <h1>We Build Brand</h1>
+            <span>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit,sed diam nonummy
+                nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            </span>
+            <button>LEARN MORE</button>
+            <a href=""></a>
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 
 import classes from './../styles/App/App.module.css';
+import logo from './../assets/b-logo.png'
 
 export const Header: React.FC = () => {
 
 
     return (
-        <div className={classes.header}>
-            <h1>h1</h1>
+        <div className={classes.header} >
+            
+            <a href="#" className={classes.logo_wrapper}>
+                <div className={classes.logo}>
+                    <div className={classes.logo_img}>
+                        <img src={logo} alt="logo"/>
+                    </div>
+
+                    <div className={classes.logo_title}>
+                        <h2>BLA BLA</h2>
+                        <h3>One Page Flat Template</h3>
+                    </div>
+
+                </div>
+            </a>
+            
+            <div className={classes.buttons}>
+                <button>HOME</button>
+                <button>PORTFOLIO</button>
+                <button>ABOUT</button>
+                <button>CONTACT</button>
+            </div>
         </div>
         
     )
